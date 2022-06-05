4c264d9b897d2e06509f6db43d3ce368cd351494
Linda Quelex <lindamadelin@gmail.com>
2022-06-04 20:44:02 -0600

    Inicio de tarea

---
+++ CODIGO_FUENTE.java
@@ -0,0 +1,10 @@
+public class Tarea2 {
+    private final int REPETICIONES =5;
+    public static void main(String []args){
+        int i=0;
+        while(i<REPETICIONES){
+            System.out.println("Repetición NO. " + (i+1));
+            i++;
+        }
+    }
+}
